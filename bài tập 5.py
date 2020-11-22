87fd15a6d9a5cfa973eb466ece62a4eb5b5a76ea
Anh-Quan-07 <72288799+Anh-Quan-07@users.noreply.github.com>
2020-11-22 22:21:10 +0700

    Add files via upload

---
+++ bài tập 5.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 22 20:51:31 2020
+
+@author: quann
+"""
+import math
+x= [1, 3, 24, 16, 100, 2,4, 1,9]
+for pt in x:
+      print("các phần tử trong list" ,pt)
+for i in range(len(x)):
+       print("Các số đc logarit là ",math.log10(x[i]))
+                                    
+    
+    
+    
