35ed37f8a3c1ceac5dc6ac036daa71ebcc89e4ba
Anh-Quan-07 <72288799+Anh-Quan-07@users.noreply.github.com>
2020-11-24 14:53:22 +0700

    Add files via upload

---
+++ bài tập 8.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 24 14:39:14 2020
+
+@author: quann
+"""
+
+h=int(input("Nhập số lần bạn muốn gửi tới = "))
+i=0
+i=int(i)
+while i<h:
+     a = (input("Nhập địa chỉ gmail bạn muốn gửi  "));
+     b = (input("Nhập thông điệp bạn muốn gửi   "));
+     i+=1 
+print("kết thúc vòng lặp vs h lần gửi !!!")     
